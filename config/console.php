767e7fb5d192c4092e7c8e5b79b0dae955eb7a86
Mark Wadham <mark.wadham@openeyes.org.uk>
2013-02-19 15:15:08 +0000

    made the console config automatically return commandMap definitions for all the commands.

--- config/console.php
+++ config/console.php
@@ -1,103 +1,6 @@
 <?php
-return array(
-	'commandMap' => array(
-		'checkfirms' => array(
-			'class' => 'application.modules.MEHCommands.commands.CheckFirmsCommand',
-		),
-		'checkintegrity' => array(
-			'class' => 'application.modules.MEHCommands.commands.CheckIntegrityCommand',
-		),
-		'consolidatemigrations' => array(
-			'class' => 'application.modules.MEHCommands.commands.ConsolidateMigrationsCommand',
-		),
-		'correspondencedata' => array(
-			'class' => 'application.modules.MEHCommands.commands.CorrespondenceDataCommand',
-		),
-		'erodrules' => array(
-			'class' => 'application.modules.MEHCommands.commands.ErodRulesCommand',
-		),
-		'exportdata' => array(
-			'class' => 'application.modules.MEHCommands.commands.ExportDataCommand',
-		),
-		'fetchusers' => array(
-			'class' => 'application.modules.MEHCommands.commands.FetchUsersCommand',
-		),
-		'firmsecretaryfaxnumbers' => array(
-			'class' => 'application.modules.MEHCommands.commands.FirmSecretaryFaxNumbersCommand',
-		),
-		'fixsites' => array(
-			'class' => 'application.modules.MEHCommands.commands.FixSitesCommand',
-		),
-		'generatesessions' => array(
-			'class' => 'application.modules.MEHCommands.commands.GenerateSessionsCommand',
-		),
-		'grepletters' => array(
-			'class' => 'application.modules.MEHCommands.commands.GrepLettersCommand',
-		),
-		'housekeeping' => array(
-			'class' => 'application.modules.MEHCommands.commands.HousekeepingCommand',
-		),
-		'importconsultants' => array(
-			'class' => 'application.modules.MEHCommands.commands.ImportConsultantsCommand',
-		),
-		'importcontacts' => array(
-			'class' => 'application.modules.MEHCommands.commands.ImportContactsCommand',
-		),
-		'importdata' => array(
-			'class' => 'application.modules.MEHCommands.commands.ImportDataCommand',
-		),
-		'importepatientletters2' => array(
-			'class' => 'application.modules.MEHCommands.commands.ImportEpatientletters2Command',
-		),
-		'importepatientletterssincelaunch' => array(
-			'class' => 'application.modules.MEHCommands.commands.ImportEPatientLettersSinceLaunchCommand',
-		),
-		'lettergrepper' => array(
-			'class' => 'application.modules.MEHCommands.commands.LetterGrepperCommand',
-		),
-		'opnotedata' => array(
-			'class' => 'application.modules.MEHCommands.commands.OpnoteDataCommand',
-		),
-		'opnoteoperationlink' => array(
-			'class' => 'application.modules.MEHCommands.commands.OpnoteOperationLinkCommand',
-		),
-		'optometrymacros' => array(
-			'class' => 'application.modules.MEHCommands.commands.OptometryMacrosCommand',
-		),
-		'orthopticmacros' => array(
-			'class' => 'application.modules.MEHCommands.commands.OrthopticMacrosCommand',
-		),
-		'rbac' => array(
-			'class' => 'application.modules.MEHCommands.commands.RbacCommand',
-		),
-		'relatedimportcomplex' => array(
-			'class' => 'application.modules.MEHCommands.commands.RelatedImportComplexCommand',
-		),
-		'report' => array(
-			'class' => 'application.modules.MEHCommands.commands.ReportCommand',
-		),
-		'test5' => array(
-			'class' => 'application.modules.MEHCommands.commands.Test5Command',
-		),
-		'test6' => array(
-			'class' => 'application.modules.MEHCommands.commands.Test6Command',
-		),
-		'lettercontactrules' => array(
-			'class' => 'application.modules.MEHCommands.commands.LetterContactRulesCommand',
-		),
-		'letterwarningrules' => array(
-			'class' => 'application.modules.MEHCommands.commands.LetterWarningRulesCommand',
-		),
-		'migratebooking' => array(
-			'class' => 'application.modules.MEHCommands.commands.MigrateBookingCommand',
-		),
-		'operationnamerules' => array(
-			'class' => 'application.modules.MEHCommands.commands.OperationNameRulesCommand',
-		),
-		'waitinglistcontactrules' => array(
-			'class' => 'application.modules.MEHCommands.commands.WaitingListContactRulesCommand',
-		),
-	),
+$config = array(
+	'commandMap' => array(),
 	'params' => array(
 		'import_contacts_username' => '',
 		'import_contacts_password' => '',
@@ -107,3 +10,15 @@ return array(
 		'epatient_database' => '',
 	),
 );
+
+$dh = opendir(dirname(__FILE__)."/../commands");
+
+while ($file = readdir($dh)) {
+	if (preg_match('/^(.*?)Command\.php$/',$file,$m)) {
+		$config['commandMap'][strtolower($m[1])] = array(
+			'class' => "application.modules.MEHCommands.commands.{$m[1]}",
+		);
+	}
+}
+
+return $config;
